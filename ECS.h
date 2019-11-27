5d2946f22a81c624c39f20795173becccf8f384c
as05016 <54739703+as05016@users.noreply.github.com>
2019-11-27 14:35:36 +0500

    Update ECS.h

--- ECS.h
+++ ECS.h
@@ -1,6 +1,6 @@
 #pragma once
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 #include <memory>
 #include <algorithm>
 #include <bitset>
@@ -9,101 +9,105 @@
 class Component;
 class Entity;
 
-using ComponentID= std::size_t;
+using ComponentID = std::size_t;
 inline ComponentID getComponentTypeID()
 {
-    static ComponentID lastID=0;
+    static ComponentID lastID = 0;
     return lastID++;
 }
-template <typename T> inline ComponentID getComponentTypeID() noexcept
+template <typename T>
+inline ComponentID getComponentTypeID() noexcept
 {
-    static ComponentID typeID=getComponentTypeID();
+    static ComponentID typeID = getComponentTypeID();
     return typeID;
 }
-constexpr std::size_t maxComponents=32;
-using ComponentBitSet = std:: bitset<maxComponents>;
-using ComponentArray = std::array<Component*,maxComponents>;
+constexpr std::size_t maxComponents = 32;
+using ComponentBitSet = std::bitset<maxComponents>;
+using ComponentArray = std::array<Component *, maxComponents>;
 
 class Component
 {
-    public:
-    Entity* entity;
-    virtual void init(){}
-    virtual void update(){}
-    virtual void draw(){}
-    virtual ~Component(){}
+public:
+    Entity *entity;
+    virtual void init() {}
+    virtual void update() {}
+    virtual void draw() {}
+    virtual ~Component() {}
 };
 
 class Entity
 {
 private:
-    bool active =true;
-    std::vector<std::unique_ptr<Component>>components;
+    bool active = true;
+    std::vector<std::unique_ptr<Component>> components;
     ComponentArray componentArray;
     ComponentBitSet ComponentBitSet;
 
 public:
     void update()
     {
-        for (auto& c: components) c->update();
-        for (auto& c: components) c->draw();
+        for (auto &c : components)
+            c->update();
+        for (auto &c : components)
+            c->draw();
     }
-    void draw(){}
-    bool isActive() const{return active;} 
-    void destroy(){active =false;}   
+    void draw() {}
+    bool isActive() const { return active; }
+    void destroy() { active = false; }
 
-    template< typename T> bool hasComponent() const
+    template <typename T>
+    bool hasComponent() const
     {
         return ComponentBitSet[getComponentID<T>];
     }
-    template<typename T, typename...TArgs>
-    T& addcomponent(TArgs&&...mArgs)
+    template <typename T, typename... TArgs>
+    T &addcomponent(TArgs &&... mArgs)
     {
-        T* c(new T(std::forward<TArgs>(mArgs)...));
-        c->entity=this;
-        std::unique_ptr<Component>uPtr{c};
+        T *c(new T(std::forward<TArgs>(mArgs)...));
+        c->entity = this;
+        std::unique_ptr<Component> uPtr{c};
         components.emplace_back(std::move(uPtr));
-        componentArray[getComponentTypeID<T>()]= c;
-        ComponentBitSet[getComponentTypeID<T>()]=true;
+        componentArray[getComponentTypeID<T>()] = c;
+        ComponentBitSet[getComponentTypeID<T>()] = true;
         c->init();
-        return *c; 
+        return *c;
     }
-    template<typename T> T& getComponent() const
+    template <typename T>
+    T &getComponent() const
     {
-        auto ptr (ComponentArray[getComponentTypeID<T>()]);
-        return *static_cast<T*>(ptr);
+        auto ptr(ComponentArray[getComponentTypeID<T>()]);
+        return *static_cast<T *>(ptr);
     }
-   // gameobject.getComponent<PositionComponent>()setXpos(25); 
-
-
+    // gameobject.getComponent<PositionComponent>()setXpos(25);
 };
 class Manager
 {
 private:
-    std::vector<std::unique_ptr<Entity>>entities;
+    std::vector<std::unique_ptr<Entity>> entities;
+
 public:
     void update()
     {
-        for (auto& e:entities) e->update();
+        for (auto &e : entities)
+            e->update();
     }
     void draw()
     {
-        for (auto& e:entities) e->draw();
+        for (auto &e : entities)
+            e->draw();
     }
     void refresh()
     {
-        entities.erase(std::remove_if(std::begin(entities),std::end(entities),[](const std::unique_ptr<Entity>&mEntity)
-        {
-            return !mEntity-> isActive();
-        }),
-        std::end(entities));
+        entities.erase(std::remove_if(std::begin(entities), std::end(entities), [](const std::unique_ptr<Entity> &mEntity) {
+                           return !mEntity->isActive();
+                       }),
+                       std::end(entities));
     }
-    Entity& addEntity()
+    Entity &addEntity()
     {
-        Entity* e=new Entity();
-        std::unique_ptr<Entity>uPtr{ e };
+        Entity *e = new Entity();
+        std::unique_ptr<Entity> uPtr{e};
         entities.emplace_back(std::move(uPtr));
         return *e;
     }
-
 };
